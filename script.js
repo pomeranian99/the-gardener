b3a82189a97f21eeb64640874cb318d1d870b669
Glitch (hello-webpage) <none>
2019-02-13 19:15:51 +0000

    ⛽️👨 Checkpoint ./script.js:2990682/245 ./.glitch-assets:2990682/389 ./index.html:2990682/53

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
-
-// prints "hi" in the browser's dev tools console
-console.log('hi');
+function setup() {
+  createCanvas(400,300);
+  background(25);
+}
