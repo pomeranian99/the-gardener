707144010f81d3332d6945adcbe78b2a22ddd28f
Glitch (hello-webpage) <none>
2019-02-13 19:47:07 +0000

    🛎🏣 Checkpoint ./script.js:2990682/497

--- script.js
+++ script.js
@@ -4,12 +4,23 @@ function setup() {
 }
 function draw() {
   background(25);
-  fill(255,255,255);
-  ellipse(width/2, height/2, 100, 100);
+  stroke(255,255,255);
+ line(mouseX,mouseY,pmouseX, pmouseY,);
+  line(width - mouseX, mouseY, width - pmouseX, mouseY);
+  
+} 
+  
+/*ellipse(width/2, height/2, 100, 100);
   fill(64);
   ellipse(180,140,20,20);
   ellipse(220,140,20,20);
   fill(196);
-  ellipse(constrainmouseX,140,10,20);
-  ellipse(mouseX+40,140,10,20);
-}
+  ellipse(constrain(mouseX - 20,172,190),
+          140,
+          10,
+          20);
+  ellipse(constrain(mouseX + 20,212,228),
+          140,
+          10,
+          20);
+          */
