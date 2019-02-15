4adc10cffef9f049750d7651961847d8c0d7cb35
Glitch (hello-webpage) <none>
2019-02-15 22:29:04 +0000

    🏓🐋 Checkpoint ./script.js:2990682/3577

--- script.js
+++ script.js
@@ -1,38 +1,17 @@
-let angle = 0.0;
-function setup() {
-  createCanvas(100, 100, WEBGL);
+function setup(){
+	createCanvas(400, 400);
 }
+function draw(){
+	background(240);
 
-function draw() {
-  background(200);
-if (second() % 2 === 0) {
-let c = cos(angle);  
-}
-  //increase the angle value using the most recent jitter value
-  
-  //use cosine to get a smooth CW and CCW motion when not jittering
-  
-  //move the shape to the center of the canvas
-  translate(width / 2, height / 2);
-  //apply the final rotation
-  rotate(c);
-  rect(0, 0, 180, 180);
-}
+	// move the origin to the pivot point
 
-}
 
-  
-/*ellipse(width/2, height/2, 100, 100);
-  fill(64);
-  ellipse(180,140,20,20);
-  ellipse(220,140,20,20);
-  fill(196);
-  ellipse(constrain(mouseX - 20,172,190),
-          140,
-          10,
-          20);
-  ellipse(constrain(mouseX + 20,212,228),
-          140,
-          10,
-          20);
-          */
+	// then rotate the grid around the pivot point by a
+	// number of degrees equal to the frame count of the sketch
+	rotate(radians(frameCount));
+
+	// and draw the square at the origin
+	fill(0);
+	rect(0, 0, 100, 100);
+}
