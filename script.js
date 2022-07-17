b64337145405cd0e97306a031643d1eeeccc54f9
Glitch (hello-webpage) <none>
2022-07-17 04:38:32 +0000

    🎸🌱 Checkpoint ./script.js:291626/329 ./index.html:291626/289

--- script.js
+++ script.js
@@ -1,18 +1,7 @@
 function setup(){
-	createCanvas(400, 400);
+	createCanvas(300, 300);
 }
 function draw(){
-	background(240);
-
-	// move the origin to the pivot point
-	translate(width/2, height/2); 
-
-	// then rotate the grid around the pivot point by a
-	// number of degrees equal to the frame count of the sketch
-	rotate(radians(frameCount));
-
-	// and draw the square at the origin
-	fill(0);
-  circle(0,0,5);
-	rect(10, 10, 100, 100);
+	background(0);
+  
 }
