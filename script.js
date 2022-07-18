53172751f95a3ef2e5b6934b9b8b22aee457c749
Glitch (hello-webpage) <none>
2022-07-18 21:47:06 +0000

    🌵⛑ Checkpoint ./script.js:291626/438

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
+var canva = 60;
+var cellSize = 5;
+var cell = [];
+var mainLocX = canva * cellSize / 2;
+var mainLocY = canva * cellSize / 2;
+
 function setup(){
-	createCanvas(300, 300);
+	createCanvas(canva * cellSize, canva * cellSize);
 }
+
+
 function draw(){
 	background(0);
+  noStroke();
+  fill(51, 119, 255);
+  rect(mainLocX, mainLocY, cellSize, cellSize);
   
 }
