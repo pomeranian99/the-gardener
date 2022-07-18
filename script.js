5906f8956be8b55ad1c1800ed57c4a94a1f0bb23
Glitch (hello-webpage) <none>
2022-07-18 21:59:01 +0000

    👦👰 Checkpoint ./script.js:291626/643

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
-var canva = 60;
-var cellSize = 5;
-var cell = [];
-var mainLocX = canva * cellSize / 2;
-var mainLocY = canva * cellSize / 2;
+var canva = 60; // number of rows and columns 
+var cellSize = 5; // size of each cell // 
+var flowerList = [];
+var mainLocX = canva * cellSize / 2; // starting position
+var mainLocY = canva * cellSize / 2; // starting position
+var waitPlant = 15000; // how long to wait before planting a new flower
+var planter
+
+var timeNow = millis(); // starting time for entire simulation
 
 function setup(){
 	createCanvas(canva * cellSize, canva * cellSize);
 }
 
-
 function draw(){
+  clear();
 	background(0);
   noStroke();
   fill(51, 119, 255);
   rect(mainLocX, mainLocY, cellSize, cellSize);
-  
+  // if it's time to plant, plant!
+  if (millis() - timeNow > waitPlant) {
+    
+  }
 }
